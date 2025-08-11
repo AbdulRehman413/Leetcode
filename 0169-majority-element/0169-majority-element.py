04abb9fa175ca10d0aedd09245f64201170a3adb
Abdul Rehman Manzoor <abdul.rehman51048@gmail.com>
2025-08-12 00:48:13 +0500

    Time: 8 ms (37.2%), Space: 19.3 MB (50.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = []
-        l = 0
-        for r in range(1, len(nums)):
-            if nums[l] == nums[r]:
-                dic.append(nums[l])
-        
-        # Remove duplicates
-        new_dic = set(dic)
+        count = 0
+        candidate = None
 
-        # If you still want it as an integer
-        final_number = int(''.join(map(str, sorted(new_dic))))
-        return final_number
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
