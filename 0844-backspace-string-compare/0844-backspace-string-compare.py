79b5ece376db527795038a0a16e2ee87da4c4b6c
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-08-29 22:17:48 +0500

    Time: 0 ms (100%), Space: 17.9 MB (36.39%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,42 +1,54 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
+            stack = []
+            stack2 = []
+            for i in s:
+                if i == "#":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(i)
+            stack= ''.join(stack)
+
+            for i in t:
+                if i == "#":
+                    if stack2:
+                        stack2.pop()
+                else:
+                    stack2.append(i)
+            stack2 = ''.join(stack2)
+
+            return stack == stack2
+    
+
         # s = list(s)
-   
-        # stack = []
-        # stack.append(s)
-        # for i in range(len(s)):
-        #     if i == "#":
-        #         stack.pop()
+        # i= 0
+        # while i <len(s):
+        #     if s[i] == "#":
+        #         s.pop(i)
+        #         if i > 0:
+
+        #             s.pop(i-1)
+        #             i-=1
         #     else:
-        #         return 1
-        s = list(s)
-        i= 0
-        while i <len(s):
-            if s[i] == "#":
-                s.pop(i)
-                if i > 0:
-
-                    s.pop(i-1)
-                    i-=1
-            else:
-                i+=1
-        s = ''.join(s)
+        #         i+=1
+        # s = ''.join(s)
 
 
-        t = list(t)
-        j = 0
-        while j < len(t):
-            if t[j] == "#":
-                t.pop(j)
-                if j > 0:
+        # t = list(t)
+        # j = 0
+        # while j < len(t):
+        #     if t[j] == "#":
+        #         t.pop(j)
+        #         if j > 0:
                     
-                    t.pop(j-1)
-                    j-=1
-            else:
-                j+=1
-        t = ''.join(t)
+        #             t.pop(j-1)
+        #             j-=1
+        #     else:
+        #         j+=1
+        # t = ''.join(t)
 
-        return s == t
+        # return s == t
             
 
                
