04c2b7cc302f6179ab5e0cabd5c704a16c231d01
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-09-07 00:14:35 +0500

    Time: 0 ms (100%), Space: 17.9 MB (26.49%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,25 +1,37 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        hashh = {}
-        for i in s:
-            hashh[i] = hashh.get(i,0) +1
-        stack = []
-        instack = set()
-        for i , c in enumerate(s) :
-            hashh[c] -=1
+        last = {c:i for i,c in enumerate(s)} #last occurrence
+        stack=[]
+        seen= set()
 
-            if c in stack:
+        for i,c in enumerate(s):
+            if c in seen:
                 continue
+            while stack and c < stack[-1] and i < last[stack[-1]]:
+                seen.remove(stack.pop())
+            stack.append(c)
+            seen.add(c)
+        return ''.join(stack)
+        # hashh = {}
+        # for i in s:
+        #     hashh[i] = hashh.get(i,0) +1
+        # stack = []
+        # instack = set()
+        # for i , c in enumerate(s) :
+        #     hashh[c] -=1
+
+        #     if c in stack:
+        #         continue
 
         
-            while stack and stack[-1] > c and hashh[stack[-1]] > 0:
-                removed = stack.pop()
-                instack.remove(removed)
+        #     while stack and stack[-1] > c and hashh[stack[-1]] > 0:
+        #         removed = stack.pop()
+        #         instack.remove(removed)
 
-            stack.append(c)
-            instack.add(c)
+        #     stack.append(c)
+        #     instack.add(c)
 
-        return ''.join(stack)
+        # return ''.join(stack)
 
             
         
