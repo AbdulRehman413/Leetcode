7509540474d669c7b9e5c538cf6812afd69fd8ec
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-09-07 20:53:02 +0500

    Time: 130 ms (26.85%), Space: 33.6 MB (23.71%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,27 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        stack = []
         n = len(temperatures)
         ans = [0] * n
-        stack = []   # will store (temperature, index)
-
         for i in range(n):
-            # check if today's temperature is warmer than the last one in stack
             while stack and temperatures[i] > stack[-1][0]:
-                temp, idx = stack.pop()
+                temp, idx  =stack.pop()
                 ans[idx] = i - idx
-            stack.append((temperatures[i], i))
-        
-        return ans
-        # stack = []
-        # n = len(temperatures)
-        # ans = [0] * n
-        # for i in range(n):
-        #     while stack and temperatures[i] > stack[-1][0]:
-        #         temp, idx  =stack.pop()
-        #         ans[idx] = i - idx
-        #         stack.append((temperatures[i],i))
+            stack.append((temperatures[i],i))
 
-        # return ans 
+        return ans 
 
 
         
