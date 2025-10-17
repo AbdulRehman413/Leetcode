38eeb099b9722b4c97b9f15762760486514012c8
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-10-17 18:34:18 +0500

    Time: 14 ms (69.37%), Space: 14.8 MB (10.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,21 +4,11 @@ class Solution(object):
         for i in s:
             if i.isalnum():
                 temp.append(i.lower())
-        clean = ''.join(temp)
+        merge = ''.join(temp)
         reverse = temp[::-1]
         if reverse == temp:
             return True
         return False
-
-        # temp = []
-        # for i in s:
-        #     if i.isalnum():
-        #         temp.append(i.lower())
-        #     merge = ''.join(temp)
-        #     reverse = temp[::-1]
-        #     if reverse == temp:
-        #         return True
-        #     return False
        
        
 
