2fc9d9d40d5d9ab97aab40dc0b1bae1e7e60859a
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-10-17 18:19:13 +0500

    Time: 13 ms (72.86%), Space: 15.4 MB (5.72%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        temp = []
+        for i in s:
+            if i.isalnum():
+                temp.append(i.lower())
+        clean = '',join(temp)
+        reverse = temp[::-1]
+        if reverse == temp:
+            return True
+        return False
+
+        
