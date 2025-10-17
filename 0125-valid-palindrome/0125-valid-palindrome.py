72251f51b7cb661042e018df658028d8ea1f458a
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-10-17 21:21:25 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def isPalindrome(self, s):
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         temp = []
         for i in s:
             if i.isalnum():
                 temp.append(i.lower())
-        merge = ''.join(temp)
-        reverse = temp[::-1]
-        if reverse == temp:
-            return True
-        return False
-       
-       
-
+            merge = ''.join(temp)
+            reverse = temp[::-1]
+            if reverse == temp:
+                return True
+            return False
         
