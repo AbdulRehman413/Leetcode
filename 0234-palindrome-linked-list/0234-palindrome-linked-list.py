2c7cbd4d7a525659e5a9086fcf60f3dd8f216dcd
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-11-06 21:43:47 +0500

    Time: 135 ms (43.18%), Space: 66.5 MB (63.25%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,24 +5,36 @@
 #         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
-        slow = fast = head
+        #  midldle of linked list
+        slow = head
+        fast = head 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        prev = None
+
+        # Reverse LL
+
+        prev = None 
         while slow:
             nxt = slow.next
             slow.next = prev
-            prev = slow
+            prev = slow 
             slow = nxt
-        
- 
-        left, right = head, prev
-        while right: 
+
+        # Comparison
+
+        left = head
+        right = prev 
+        while right:
             if left.val != right.val:
                 return False
             left = left.next
             right = right.next
-        
+
         return True
+
+        
+
+
+
+        
