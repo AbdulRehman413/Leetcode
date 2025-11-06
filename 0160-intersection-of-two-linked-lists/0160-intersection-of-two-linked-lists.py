a4bab8841fdaed95c0f8b6e512b1cda82bc50e54
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-11-06 19:07:56 +0500

    Time: 90 ms (29.21%), Space: 27.9 MB (47.57%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -9,12 +9,13 @@ class Solution:
         if not headA or not headB:
             return None
         
-        a, b = headA, headB
+        a= headA
+        b = headB
         
         while a != b:
             a = a.next if a else headB
             b = b.next if b else headA
             
         return a
-        
+
         
