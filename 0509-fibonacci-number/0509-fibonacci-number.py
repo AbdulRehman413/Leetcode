529a1e70c20f91c25e63723ba5fc911f4eedd767
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-11-19 21:31:06 +0500

    Time: 7 ms (98.1%), Space: 12.5 MB (10.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -9,12 +9,12 @@ class Solution(object):
         if n == 1:
             return 1
 
-        dp = [0] * (n+1)
-        dp[0] = 0
-        dp[1] = 1
+        prev = 0
+        curr = 1
 
         for i in range(2,n+1):
-            dp[i] = dp[i-2]  + dp[i-1]
-        return dp[n]
+           prev, curr = curr, prev + curr
+           
+        return curr
 
         
